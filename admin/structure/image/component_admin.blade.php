450b0d696c8e74940185623c6553e3953ef06a08
reindert-vetter <reindertvetter@gmail.com>
2024-04-29 22:17:55 +0200

    Cropper with basic croptool

--- admin/structure/image/component_admin.blade.php
+++ admin/structure/image/component_admin.blade.php
@@ -1,42 +1,69 @@
 @php /** @var \Confetti\Components\Image $model */ @endphp
 
 <image-component
-    data-name="{{ $model->getId() }}"
-    data-label="{{ $model->getComponent()->getLabel() }}"
-    data-help="{{ $model->getComponent()->getDecoration('help') }}"
-    data-value=@json($model->get())
+        data-name="{{ $model->getId() }}"
+        data-label="{{ $model->getComponent()->getLabel() }}"
+        data-help="{{ $model->getComponent()->getDecoration('help') }}"
+        data-value=@json($model->get())
 ></image-component>
 
-
+@pushonce('styles_cropper')
+    <link rel="stylesheet" href="https://esm.sh/cropperjs@^1/dist/cropper.css">
+@endpushonce
 @pushonce('end_of_body_image_component')
+    <style>.cropper-modal {opacity: 0.1}</style>
     <script type="module">
         import {Toolbar} from '/admin/assets/js/lim_editor.mjs';
         import {Storage, IconUpload} from '/admin/assets/js/admin_service.mjs';
         /** @see https://github.com/codex-team/icons */
         import {IconUndo} from 'https://esm.sh/@codexteam/icons';
         import {html, reactive} from 'https://esm.sh/@arrow-js/core';
-
-        const data = reactive({
-            currentFile: null,
-            dragover: false,
-        })
+        // https://fengyuanchen.github.io/cropperjs
+        import Cropper from 'https://esm.sh/cropperjs';
 
         class ImageComponent extends HTMLElement {
+            data = {
+                currentFile: null,
+                toCrop: null,
+                dragover: false,
+            };
+
+            constructor() {
+                super();
+                this.data = reactive(this.data);
+            }
+
             connectedCallback() {
                 html`
                     <label class="block text-bold text-xl mt-8 mb-4">${this.dataset.label}</label>
                     <div class="flex items-center justify-center w-full">
+
+                        ${() => this.data.toCrop ? html`
+                            <!-- Canvas to Crop the image -->
+                            <div class="w-full h-64 border-2 border-gray-300 border-solid rounded-lg">
+                                <div class="relative h-64 overflow-hidden rounded-lg">
+<img class="absolute w-full h-64 object-cover blur-md" src="${() => this.data.toCrop ? URL.createObjectURL(this.data.toCrop) : ''}"
+                                         alt="">
+                                    <img id="image" style="display: block;max-width: 100%;"
+                                         src="${() => this.data.toCrop ? URL.createObjectURL(this.data.toCrop) : ''}"
+                                         onload="${() => setTimeout(() => this.#loadCropper(this.querySelector('#image')), 1)}"
+                                         >
+                                </div>
+                            </div>
+                        ` : ``}
                         <label for="${this.dataset.name}"
-                               class="${() => `_dropzone flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 ${data.dragover ? `border-solid` : `border-dashed`} rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100`}">
-                            <!-- current image -->
-                            ${() => data.currentFile ? html`
+                               class="${() => `_dropzone ${this.data.toCrop ? `hidden` : ``} flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 ${this.data.dragover ? `border-solid` : `border-dashed`} rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100`}">
+                            ${() => this.data.currentFile ? html`
+                                <!-- current image -->
                                 <div class="w-full h-64">
                                     <img class="w-full h-64 object-cover rounded-lg"
-                                         src="${() => data.currentFile ? URL.createObjectURL(data.currentFile) : ''}"
+                                         src="${() => this.data.currentFile ? URL.createObjectURL(this.data.currentFile) : ''}"
                                          alt=""
                                     />
                                 </div>
-                            ` : html`
+                            ` : ``}
+
+                            ${() => !this.data.currentFile ? html`
                                 <!-- Information for new image -->
                                 <div class="flex flex-col items-center justify-center pt-5 pb-6">
                                     ${IconUpload(`w-8 h-8 mb-4 text-gray-500`)}
@@ -46,46 +73,72 @@
                                     <p class="text-xs text-gray-500">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
                                     <p class="text-xs text-gray-500">${this.dataset.help}</p>
                                 </div>
-                            `}
-                            <input @change="${() => (data.currentFile = this.querySelector('input').files[0])}"
+                            ` : ``}
+                            <input @change="${() => (this.data.toCrop = this.querySelector('input').files[0])}"
                                    id="${this.dataset.name}"
                                    type="file"
                                    accept="image/*"
                                    class="hidden"
                                    multiple="false"
-
                             />
                         </label>
+
                     </div>
                 `(this)
+
                 this.#addListeners();
+            }
+
+            #loadCropper(element) {
+                new Cropper(element, {
+                    // aspectRatio: 16 / 9,
+                    aspectRatio: 1 / 1,
+                    background: false,
+                    modal: true,
+                    guides: false,
+                    restore: false,
+                    dragMode: 'none',
+                    center: false,
+                    viewMode: 2,
+                    autoCropArea: 1,
+                    crop(event) {
+                        console.log(event.detail.x);
+                        console.log(event.detail.y);
+                        console.log(event.detail.width);
+                        console.log(event.detail.height);
+                        console.log(event.detail.rotate);
+                        console.log(event.detail.scaleX);
+                        console.log(event.detail.scaleY);
+                    },
+                });
             }
 
             #addListeners() {
                 this.querySelectorAll('._dropzone').forEach(input => {
-                    input.addEventListener('dragover', function(e) {
+                    data = this.data;
+                    input.addEventListener('dragover', function (e) {
                         data.dragover = true;
                         console.log('dragover');
                         e.preventDefault();
                         e.stopPropagation();
                     });
 
-                    input.addEventListener('dragleave', function(e) {
+                    input.addEventListener('dragleave', function (e) {
                         data.dragover = false;
                         console.log('dragleave');
                         e.preventDefault();
                         e.stopPropagation();
                     });
 
-                    input.addEventListener('drop', function(e) {
+                    input.addEventListener('drop', function (e) {
                         data.dragover = false;
                         console.log('drop');
                         e.preventDefault();
                         e.stopPropagation();
-                        if(e.dataTransfer) {
-                            data.currentFile = e.dataTransfer.files[0];
-                        } else if(e.target) {
-                            data.currentFile = e.target.files[0];
+                        if (e.dataTransfer) {
+                            data.toCrop = e.dataTransfer.files[0];
+                        } else if (e.target) {
+                            data.toCrop = e.target.files[0];
                         }
                     });
                 });
